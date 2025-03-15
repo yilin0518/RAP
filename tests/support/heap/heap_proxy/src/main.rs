55dbd095ac74879586c6eab8425b2d4f2232b81c
aisr <china.xuhui@gmail.com>
2025-03-15 11:43:24 +0800

    update a heap test case

--- tests/support/heap/heap_proxy/src/main.rs
+++ tests/support/heap/heap_proxy/src/main.rs
@@ -1,18 +1,30 @@
 use std::marker::PhantomData;
 
+//expected result: (0,[0])
 struct Proxy1<T> {
     _p: *mut T,
 }
 
-
+//expected result: (1,[0])
 struct Proxy2<T> {
     _p: *mut T,
     _marker: PhantomData<T>,
 }
 
+//expected result: (0,[0,0])
 struct Proxy3<'a, T> {
     _p: *mut T,
     _marker: PhantomData<&'a T>,
+}
+
+//expected result: (0,[1])
+struct Proxy4<T> {
+    _x: T,
+}
+
+//expected result: (1,[0])
+struct Proxy5<T> {
+    _x: Proxy2<T>,
 }
 
 impl<T> Drop for Proxy1<T> {
@@ -33,7 +45,10 @@ impl<'a, T> Drop for Proxy3<'a, T> {
 fn main() {
     let buf = Box::new("buffer");
     let ptr = Box::into_raw(buf);
-    let _proxy1 = Proxy1 { _p:ptr };
-    let _proxy2 = Proxy2 { _p:ptr, _marker: PhantomData };
-    let _proxy3 = Proxy3 { _p:ptr, _marker: PhantomData };
+    let mut p1 = Proxy1 { _p:ptr };
+    let mut p2a = Proxy2 { _p:ptr, _marker: PhantomData };
+    let mut p2b = Proxy2 { _p:ptr, _marker: PhantomData };
+    let p3 = Proxy3 { _p: &mut p2a as *mut Proxy2<&str>, _marker: PhantomData };
+    let p4 = Proxy4 { _x: p2a };
+    let p5 = Proxy5 { _x: p2b };
 }

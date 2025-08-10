d31ac2a4e3597bcb56139d61538de291dc1d57da
aisr <china.xuhui@gmail.com>
2025-08-10 20:13:23 +0800

    upgrade rust toolchain to 2025-08-10

--- rapx/src/analysis/utils/intrinsic_id.rs
+++ rapx/src/analysis/utils/intrinsic_id.rs
@@ -1,14 +1,14 @@
 /*
  * NOTE: This should be updated with different rustc versions.
  */
-pub const DROP: usize = 2317;
-pub const DROP_IN_PLACE: usize = 2929;
-pub const ASSUME_INIT_DROP: usize = 2156;
 pub const DEALLOC: usize = 134;
-pub const MANUALLYDROP: usize = 2121;
-pub const CALL_MUT: usize = 3937;
-pub const CLONE: usize = 3110;
-pub const COPY_TO: usize = 2868;
-pub const COPY_TO_NONOVERLAPPING: usize = 2869;
-pub const COPY_FROM: usize = 2870;
-pub const COPY_FROM_NONOVERLAPPING: usize = 2871;
+pub const MANUALLYDROP: usize = 2127;
+pub const ASSUME_INIT_DROP: usize = 2162;
+pub const DROP: usize = 2323;
+pub const COPY_TO: usize = 2874;
+pub const COPY_TO_NONOVERLAPPING: usize = 2875;
+pub const COPY_FROM: usize = 2876;
+pub const COPY_FROM_NONOVERLAPPING: usize = 2877;
+pub const DROP_IN_PLACE: usize = 2935;
+pub const CLONE: usize = 3116;
+pub const CALL_MUT: usize = 3945;
